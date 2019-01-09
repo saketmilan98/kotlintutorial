aca2b686effb0f0b765d1c2702066ef1eaf6287a
Saket Milan <saketmilan98@gmail.com>
2019-01-09 09:36:22 +0530

    Initial commit

---
+++ app/src/main/java/org/geeksforgeeks/kotlintutorial/Main2Activity.kt
@@ -0,0 +1,24 @@
+package org.geeksforgeeks.kotlintutorial
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class Main2Activity : AppCompatActivity() {
+
+    lateinit var displayName : TextView
+    var name : String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        displayName = findViewById(R.id.textView2)
+
+        val intentObject : Intent = intent
+        name= intentObject.getStringExtra("name")
+        displayName.text = "Hey $name, you have successfully navigated to second activity."
+
+    }
+}
---
+++ app/src/main/java/org/geeksforgeeks/kotlintutorial/MainActivity.kt
@@ -0,0 +1,35 @@
+package org.geeksforgeeks.kotlintutorial
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var enterName : EditText
+    lateinit var submitBtn : Button
+    lateinit var displayName : TextView
+    var name : String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        enterName =findViewById(R.id.editText)
+        submitBtn =findViewById(R.id.button)
+        displayName = findViewById(R.id.textView)
+
+        submitBtn.setOnClickListener{
+            name = enterName.text.toString()
+            //displayName.setText("Hello $name, welcome to my first Kotlin app.")
+
+            val intent : Intent = Intent(applicationContext, Main2Activity::class.java)
+            intent.putExtra("name",name)
+            startActivity(intent)
+
+        }
+    }
+}
